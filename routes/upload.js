e0b9ba11db0c36c3a943ae5c03b0567481835333
verybigorange <zhoucheng1993@foxmail.com>
2017-11-28 23:29:16 +0800

    增加上传图片功能

---
+++ routes/upload.js
@@ -0,0 +1,25 @@
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var router = express.Router();
+
+//上传图片
+router.post('/', function(req, res, next) {
+    var form = new formidable.IncomingForm();
+    var targetFile = path.join(__dirname,'../public/img/');
+    form.uploadDir = targetFile;
+    form.parse(req,function(err,fields,files){
+        if(err) throw err;
+        let filename = new Date().getTime() + "$" +files.file.name;
+        var oldpath = files.file.path;
+        var newpath = path.join(path.dirname(oldpath),filename);
+        fs.rename(oldpath,newpath,(err)=>{
+            if(err) throw err;
+            res.writeHead(200,{"Content-Type":"text/html;charset=UTF8"});
+            res.end('/api/img/'+filename);
+        })
+    });
+});
+
+module.exports = router;
