923baeb95c37ac83e44cd607a92fef98eac423c8
verybigorange <zhoucheng1993@foxmail.com>
2017-11-28 23:29:01 +0800

    增加作品的接口

--- routes/works.js
+++ routes/works.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var path = require("path");
 
 var query=require("./mysql_pool");  
 
 // 查询所有作品
 router.post('/', function(req, res, next) {
     let limitNum = req.body.limit;
-    let str = limitNum?" LIMIT "+limitNum:"";
-  query("SELECT * FROM works"+str, [1], function(err,results,fields){  
-    res.send(results);
-  });
+    let currentPage = req.body.currentPage;
+    // 当传了参数的就表示需要分页查询
+    let str = limitNum?" LIMIT "+(currentPage-1)*limitNum+","+currentPage*limitNum:"";
+    let obj = {};
+    query("SELECT count(*) FROM works", [1], function(err,results,fields){ 
+        obj.count = results[0]['count(*)'];   // 总条数
+        query("SELECT * FROM works"+str, [1], function(err,results,fields){
+            obj.list = results
+            res.send(obj);
+        });
+    });
+ 
 });
 
 
@@ -29,9 +39,9 @@ router.post('/type', function(req, res, next) {
 
 //按作品id查询
 router.post('/id', function(req, res, next) {
-    let id = req.body.id;
+    let id = req.body.id*1;
     if(id){
-        query("SELECT * FROM works WHERE work_id="+id+"", [1], function(err,results,fields){  
+        query("SELECT * FROM works WHERE work_id="+id+"", [1], function(err,results,fields){
             res.send(results);
         });
     }else{
@@ -53,8 +63,15 @@ router.post('/add', function(req, res, next) {
     let title = req.body.title;     //图片标题
     let show = req.body.show;       //是否首页展示该作品
     let count =  req.body.count;    //作品浏览量
-    query("INSERT INTO works (work_title,pic_url,work_type,home_show,view_count) VALUES ('"+title+"','"+url+"','"+type+"','"+show+"','"+count+"')", [1], function(err,results,fields){ 
-        res.send("1");
+    let pic_name = req.body.pic_name; //图片名称
+    //let id =  new Date().getTime(); //作品id
+    query("INSERT INTO works (work_title,pic_url,work_type,home_show,view_count,pic_name) VALUES (',"+title+"','"+url+"','"+type+"','"+show+"',"+count+",'"+pic_name+"')", [1], function(err,results,fields){ 
+       if(results){
+            res.send("1");
+       }else{
+           res.send("0");
+       }
+        
     });
    
 });
@@ -63,6 +80,18 @@ router.post('/add', function(req, res, next) {
 router.post('/delete', function(req, res, next) {
     let id = req.body.id;  //作品id
     query("DELETE FROM works WHERE work_id="+id+"", [1], function(err,results,fields){  
+        // let limitNum = req.body.limit;
+        // let currentPage = req.body.currentPage;
+        // let str = limitNum?" LIMIT "+(currentPage-1)*limitNum+","+currentPage*limitNum:"";
+        // let obj = {};
+        // query("SELECT count(*) FROM works", [1], function(err,results,fields){ 
+        //     obj.count = results[0]['count(*)'];   // 总条数
+        //     query("SELECT * FROM works"+str, [1], function(err,results,fields){
+        //         obj.list = results
+        //         res.send(obj);
+        //     });
+        // });
+       
         res.send("1");
     });
 });
@@ -75,10 +104,29 @@ router.post('/edit', function(req, res, next) {
     let title = req.body.title;     //图片标题
     let show = req.body.show;       //是否首页展示该作品
     let count =  req.body.count;    //作品浏览量
-    query("UPDATE works SET work_title='"+title+"',view_count='"+count+"',pic_url='"+url+"',work_type='"+type+"',home_show='"+show+"' WHERE work_id="+id+"", [1], function(err,results,fields){
+    let pic_name = req.body.pic_name; //图片名称
+    query("UPDATE works SET work_title='"+title+"',view_count='"+count+"',pic_url='"+url+"',work_type='"+type+"',home_show='"+show+"',pic_name='"+pic_name+"' WHERE work_id="+id+"", [1], function(err,results,fields){
         res.send("1");
     });
 });
 
+
+//把上传了的图片删除
+router.post('/delete_pic', function(req, res, next) {
+    let pic_name = req.body.pic_name;  //数据库中的图片名称
+    let id =  req.body.id;    //作品id
+    var url = path.join(__dirname,"../public/img/" + pic_name);
+    fs.unlink(url, (err) => {
+        if (err) throw err;
+        //如果id存在，删除数据库中的图片url
+        if(id){
+            query("UPDATE works SET pic_url='' WHERE work_id="+id+"", [1], function(err,results,fields){
+                res.send("1");
+            });
+        }else{
+            res.send("1");
+        }
+    });
+});
 
 module.exports = router;
